d3f860ce89b5c570924aad80e62ce139034e6204
Tavish Pegram <tapegram@gmail.com>
2016-12-26 13:58:02 -0600

    Updated config for mongo

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    'mongoUrl' : 'mongodb://localhost:27017/durandal',
+    'mongoUrl' : process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/durandal-api'
 }
